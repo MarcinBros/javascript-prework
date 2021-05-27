4a8ad4f98449ce31f8af552d014fa8cfb934c26d
Marcin Bros <marcinbp2@gmail.com>
2021-05-27 13:23:00 +0100

    Add if else & else to computer move

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}  else if (randomNumber == 2){
+    computerMove = 'papier';
+}  else (randomNumber == 3){
+    computerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
 }
 
-let computerMove = `kamień`;
+printMessage('Twój ruch to: ' + playerMove);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = 'papier';
-
-printMessage('Zagrałeś' + playerMove + 'Jeśli mój ruch to kamień to wygrywasz!');
